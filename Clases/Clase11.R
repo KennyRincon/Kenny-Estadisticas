79e38117d52537b7633fd0125d527f526ce7c954
kenny rincon <Kenny_Margarita_Lopez@hotmail.com>
2022-05-20 12:08:44 -0500

    new

--- Clases/Clase11.R
+++ Clases/Clase11.R
@@ -4,21 +4,28 @@
 datos <- read.csv("Clases/mainproduccion.csv", header = T)
 datos$Tiempo <- as.factor(datos$Tiempo)
 
-boxplot(datos$Kgsem ~ datos$Tiempo, col = "blue",
-        xlab = "Tiempo",
-        ylab = "Semilla (Kg)")
-t.test(datos$Kgsem ~ datos$Tiempo, paired =TRUE)
+# H0 = no hay diferencia entre la media de los pesos entre las semillas entre el año 2012 y el año 2013.
+# H1 = hay diferencia entre la media de los pesos entre las semillas entre el año 2012 y el año 2013.
 
-boxplot(datos$BioRama ~ datos$Tiempo, col = "pink",
+boxplot(datos$Kgsem ~ datos$Tiempo, col = "lightblue",
+        xlab = "Tiempo",
+        ylab = "Semilla (kg)")
+t.test(datos$Kgsem ~ datos$Tiempo, paired = T)
+
+
+boxplot(datos$BioRama ~ datos$Tiempo, col = "violet",
         xlab = "Tiempo",
         ylab = "BioRama (Ton)")
-        
-t.test(datos$BioRama ~ datos$Tiempo, paired =TRUE)
+t.test(datos$BioRama ~ datos$Tiempo, paired = T)
+# se rechaza la hipotesis nula (H0)
 
-#Rechaza la nula y se acerca a la alternativa
 
-boxplot(datos$Germ ~ datos$Tiempo, col = "pink",
+boxplot(datos$Germ ~ datos$Tiempo, col="pink",
         xlab = "Tiempo",
-        ylab = "Germ (%")
-t.test(datos$Germ ~ datos$Tiempo, paired =TRUE)
-        
+        ylab = "Germ (%)")
+t.test(datos$Germ ~ datos$Tiempo, paired = T)
+# se acepta la hipotesis alternativa
+
+# el numero de alumnos que ingresan a la facultad de medicina.
+# el total de carreras por temporada de los redsox
+
